e1f8ed86f9a43d10a7b1f8b306cb795d37de31b2
anowarcst <anowar.cst@gmail.com>
2020-10-07 19:35:08 +0600

    Update language

--- lang/en/quizaccess_proctoring.php
+++ lang/en/quizaccess_proctoring.php
@@ -26,34 +26,25 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-// $string['proctoringrequired_help'] = 'If you enable this option, students will not be able to start an attempt until they have ticked a check-box confirming that they are aware of the policy on webcam.';
+$string['proctoringrequired_help'] = 'If you enable this option, students will not be able to start an attempt until they have ticked a check-box confirming that they are aware of the policy on webcam.';
 $string['proctoringrequiredoption'] = 'must be acknowledged before starting an attempt';
 $string['notrequired'] = 'not required';
 $string['privacy:metadata'] = 'We do not share any personal data with third parties.';
 $string['proctoringheader'] = 'To continue with this attempt you must validate your identity using your webcam.';
 $string['proctoringlabel'] = 'I agree with the validation process.';
-// $string['proctoringwbcamrequired'] = 'Students must validate their identity with the webcam';
 $string['proctoringstatement'] = 'This exam requires webcam validation process. You must allow the webcam and it will be compared with your picture <br />(Please allow your web browser to access your camera).';
 $string['camhtml'] = '<div class="camera"> <video id="video">Video stream not available.</video></div> <canvas id="canvas" style="display:none;"> </canvas> <img style="display:none;" id="photo" alt="The screen capture will appear in this box."/>';
 $string['pluginname'] = 'Proctoring';
 $string['quizaccess_proctoring'] = 'Quizaccess Proctoring';
 $string['youmustagree'] = 'You must agree to validate your identity before continue.';
-// $string['errorvalidation'] = 'Your identity was not validated and you can not continue';
-// $string['step1snap'] = 'Snap';
-// $string['step2validate'] = 'Validate';
-// $string['identityvalidated'] = 'SUCCESS: Your identity was validated';
 $string['proctoringrequired'] = 'Webcam identity validation';
-// $string['reportlink'] = 'Webcam identity validation reports';
 $string['notpermissionreport'] = 'Identity validation reports are disabled for you.';
 $string['eprotroringreports'] = 'Identity validation report for: ';
 $string['eprotroringreportsdesc'] = 'In this report you will find all the validation attempts of the students and their status. Also, you will be able to check both pictures used by students to validate their identity, like their profile picture and webcam photo.';
 $string['status'] = 'Validation status';
 $string['dateverified'] = 'Date and time';
 $string['actions'] = 'Actions';
-// $string['statusyes'] = 'Is the same person';
-// $string['statusno'] = 'Is not the same person';
 $string['picturesreport'] = 'View proctoring report';
-// $string['webcamphoto'] = 'Webcam photo capture';
 $string['webcampicture'] = 'webcampicture';
 $string['picturesusedreport'] = 'There are the pictures used by the student to validate his/her identity';
 $string['setting:proctoringreconfigureproctoring'] = 'Auto-configure Proctoring';
@@ -74,3 +65,4 @@ $string['privacy:metadata:quizaccess_proctoring_logs'] = 'Moodle Quiz access Pro
 
 $string['proctoring:sendcamshot'] = 'Proctoring Send Webcam Photo';
 $string['proctoring:getcamshots'] = 'Proctoring Get Webcam Photos';
+$string['proctoring:viewreport'] = 'Proctoring view report';
