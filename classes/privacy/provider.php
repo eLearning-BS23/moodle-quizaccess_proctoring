1fb90b286398898e9dc4404b86f9b486a12657c8
anowarcst <anowar.cst@gmail.com>
2020-10-13 16:41:12 +0600

    remove duplicate functions

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -265,11 +265,4 @@ class provider implements
 
     }
 
-    /**
-     * Delete multiple users within a single context.
-     *
-     * @param  approved_userlist $userlist The approved context and user information to delete information for.
-     */
-    public static function delete_data_for_users(approved_userlist $userlist) {
-    }
 }
