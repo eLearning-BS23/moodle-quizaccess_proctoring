0c034469ed00471acc4d7decacce61d0cb1cf7a4
sahedbs23 <ssawon.bs23@gmail.com>
2020-10-15 16:46:53 +0600

    version updated to 2020091005->2020091006

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'quizaccess_proctoring';
 $plugin->release = '1.1.0';
-$plugin->version = 2020091005;
+$plugin->version = 2020091006;
 $plugin->requires = 2019052000;
 $plugin->maturity = MATURITY_BETA;
