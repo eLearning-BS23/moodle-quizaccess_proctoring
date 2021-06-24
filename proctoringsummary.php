9c28bd4efa0d2f8ced03326960e67dbefd26d1f8
JayaAhmed <jaya621ahmed@gmail.com>
2021-06-24 14:20:16 +0600

    proctoringsummary.php coding style fixed

--- proctoringsummary.php
+++ proctoringsummary.php
@@ -1,7 +1,22 @@
 <?php
-require_once (__DIR__ . '/../../../../config.php');
-require_once ($CFG->dirroot . '/lib/tablelib.php');
-require_once (__DIR__ . '/classes/addtional_settings_helper.php');
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+require_once(__DIR__ . '/../../../../config.php');
+require_once($CFG->dirroot . '/lib/tablelib.php');
+require_once(__DIR__ . '/classes/addtional_settings_helper.php');
 
 $cmid = required_param('cmid', PARAM_INT);
 $context = context_module::instance($cmid, MUST_EXIST);
@@ -24,7 +39,7 @@ $PAGE->set_title('Proctoring Summary Report');
 $PAGE->set_heading('Proctoring Summary Report');
 
 $PAGE->navbar->add('Proctoring Report', $url);
-$PAGE->requires->js_call_amd('quizaccess_proctoring/additionalSettings', 'setup',array());
+$PAGE->requires->js_call_amd('quizaccess_proctoring/additionalSettings', 'setup', array());
 
 echo $OUTPUT->header();
 
@@ -34,7 +49,7 @@ $coursewisesummarysql = 'SELECT
                         MQL.courseid,
                         COUNT(MQL.id) as logcount
                         FROM {quizaccess_proctoring_logs} MQL
-                        JOIN {course} MC ON MQL.courseid = MC.id  
+                        JOIN {course} MC ON MQL.courseid = MC.id
                         GROUP BY courseid,coursefullname,courseshortname';
 $coursesummary = $DB->get_records_sql($coursewisesummarysql);
 
@@ -50,21 +65,6 @@ $quizsummarysql = 'SELECT
                     GROUP BY MQ.id';
 $quizsummary = $DB->get_records_sql($quizsummarysql);
 
-//$settingsparams = array();
-//$settingsparams['section'] = 'modsettingsquizcatproctoring';
-//$mainsettingsurl = new moodle_url(
-//    '/admin/settings.php',
-//    $settingsparams
-//);
-//$title = "Summary Report";
-//$mainsettingspagebtn = get_string('mainsettingspagebtn', 'quizaccess_proctoring');
-//echo '<div>
-//        <h1>'.$title.'</h1>
-//      </div>
-//      <a class="btn btn-primary" href="'.$mainsettingsurl.'">'.$mainsettingspagebtn.'</a>
-//      <br/>
-//      <br/>
-//      ';
 
 echo '<div class="box generalbox m-b-1 adminerror alert alert-info p-y-1">'
     . get_string('summarypagedesc', 'quizaccess_proctoring') . '</div>';
@@ -78,7 +78,7 @@ echo '<table class="flexible table table_class">
 
 echo '<tbody>';
 
-foreach ($coursesummary as $row){
+foreach ($coursesummary as $row) {
     $params1 = array(
         'cmid' => $cmid,
         'type' => 'course',
@@ -88,18 +88,18 @@ foreach ($coursesummary as $row){
         '/mod/quiz/accessrule/proctoring/bulkdelete.php',
         $params1
     );
-    $deletelink1 = '<a onclick="return confirm(`Are you sure want to delete the pictures for this course?`)" href="'.$url1.'"><i class="icon fa fa-trash fa-fw "></i></a>';
+    $deletelink1 = '<a onclick="return confirm(`Are you sure want to delete the pictures for this course?`)" 
+    href="'.$url1.'"><i class="icon fa fa-trash fa-fw "></i></a>';
 
     echo '<tr class="course-row no-border">';
     echo '<td colspan="2" class="no-border">'.$row->courseshortname.":".$row->coursefullname."</td>";
-//    echo '<td>'.$row->coursefullname."</td>";
-//    echo '<td>'."</td>";
+
     echo '<td class="no-border">'.$row->logcount."</td>";
     echo '<td class="no-border">'.$deletelink1."</td>";
     echo '</tr>';
 
-    foreach ($quizsummary as $row2){
-        if($row->courseid == $row2->courseid){
+    foreach ($quizsummary as $row2) {
+        if ($row->courseid == $row2->courseid) {
             $params2 = array(
                 'cmid' => $cmid,
                 'type' => 'quiz',
@@ -109,7 +109,8 @@ foreach ($coursesummary as $row){
                 '/mod/quiz/accessrule/proctoring/bulkdelete.php',
                 $params2
             );
-            $deletelink2 = '<a onclick="return confirm(`Are you sure want to delete the pictures for this quiz?`)" href="'.$url2.'"><i class="icon fa fa-trash fa-fw "></i></a>';
+            $deletelink2 = '<a onclick="return confirm(`Are you sure want to delete the pictures for this quiz?`)" 
+            href="'.$url2.'"><i class="icon fa fa-trash fa-fw "></i></a>';
 
             echo '<tr class="quiz-row">';
             echo '<td width="5%" class="no-border"></td>';
