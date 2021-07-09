be93ce365bdd2bbb1a46202534002fc90e533236
Anowar Hossain <anowar.cst@gmail.com>
2021-07-09 18:52:46 +0600

    Update version.php

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'quizaccess_proctoring';
 $plugin->release = '1.2.1';
-$plugin->version = 2021062400;
+$plugin->version = 2021070900;
 $plugin->requires = 2019052000;
 $plugin->maturity = MATURITY_BETA;
