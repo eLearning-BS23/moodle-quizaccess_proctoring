f066548ddb0c08e14e7df6838fbe588c76025f55
SohagAhmed-23 <sohag.ahmed@brainstation-23.com>
2025-01-18 22:47:11 +0600

    fixed image  url  permission

--- lib.php
+++ lib.php
@@ -44,8 +44,10 @@ $token = "";
  * @return bool Returns false if the file cannot be found.
  */
 function quizaccess_proctoring_pluginfile($course, $cm, $context, $filearea, $args, $forcedownload, array $options = []) {
-    if (!has_capability('quizaccess/proctoring:viewreport', $context) &&  !is_siteadmin() ) {
-        throw new moodle_exception('nopermission', 'quizaccess_proctoring');
+     if ( $filearea == 'picture' || $fileare =='face_image' ) {
+        if (!has_capability('quizaccess/proctoring:viewreport', $context) &&  !is_siteadmin() ) {
+                throw new moodle_exception('nopermission', 'quizaccess_proctoring');
+            }
     }
     $itemid = array_shift($args);
     $filename = array_pop($args);
