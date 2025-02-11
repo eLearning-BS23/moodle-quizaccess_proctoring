366737bef3e4ac674b2ccbf5e895741d21678516
SohagAhmed-23 <sohag.ahmed@brainstation-23.com>
2025-02-11 17:20:27 +0600

    add navigation  menu in proctoring promo page

--- proctoring_pro_promo.php
+++ proctoring_pro_promo.php
@@ -1,76 +1,82 @@
-<?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+    <?php
+    // This file is part of Moodle - http://moodle.org/
+    //
+    // Moodle is free software: you can redistribute it and/or modify
+    // it under the terms of the GNU General Public License as published by
+    // the Free Software Foundation, either version 3 of the License, or
+    // (at your option) any later version.
+    //
+    // Moodle is distributed in the hope that it will be useful,
+    // but WITHOUT ANY WARRANTY; without even the implied warranty of
+    // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    // GNU General Public License for more details.
+    //
+    // You should have received a copy of the GNU General Public License
+    // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-/**
- * Proctoring Summary for the quizaccess_proctoring plugin.
- *
- * @package    quizaccess_proctoring
- * @copyright  2024 Brain Station 23
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later.
- */
-
-
-require_once(__DIR__.'/../../../../config.php');
-require_once($CFG->dirroot.'/mod/quiz/accessrule/proctoring/lib.php');
-
-require_login();
-$courseid = optional_param('courseid', '0', PARAM_INT);
-$cmid = optional_param('cmid' , '0' , PARAM_INT);
-
-$PAGE->set_url(new moodle_url('/mod/quiz/accessrule/proctoring/proctoring_pro_promo.php', [
-      'courseid' => $courseid, 'cmid' => $cmid]));
-$PAGE->set_context(context_system::instance());
-$PAGE->set_pagelayout('course');
-$PAGE->requires->css('/mod/quiz/accessrule/proctoring/styles.css');
-$PAGE->set_title(get_string('proctoring_pro_promo', 'quizaccess_proctoring'));
-$PAGE->navbar->add(get_string('proctoring_pro_promo', 'quizaccess_proctoring'));
-
-if ($courseid != 0 && $cmid != 0) {
-    $PAGE->navbar->add(get_string('reportpage', 'quizaccess_proctoring'),
-    new moodle_url('/mod/quiz/accessrule/proctoring/report.php', ['cmid' => $cmid , 'courseid' => $courseid]));
-    $PAGE->navbar->add('Proctoring Pro Promo');
-} else {
-    $PAGE->navbar->add(get_string('userlist', 'quizaccess_proctoring'),
-    new moodle_url('/mod/quiz/accessrule/proctoring/userslist.php'));
-    $PAGE->navbar->add('Proctoring Pro Promo');
-}
-
-echo $OUTPUT->header();
-
-$featuresimageurl = $OUTPUT->image_url('proctoring_pro_features', 'quizaccess_proctoring');
-$proctoringprologo = $OUTPUT->image_url('proctoring_pro_logo', 'quizaccess_proctoring');
-$proctoringprogif = $OUTPUT->image_url('proctoring_pro_report', 'quizaccess_proctoring');
-$proctoringgif = $OUTPUT->image_url('proctoring_report', 'quizaccess_proctoring');
-$successsign = $OUTPUT->image_url('success', 'quizaccess_proctoring');
-$crossign = $OUTPUT->image_url('crossign', 'quizaccess_proctoring');
-
-$template = 'quizaccess_proctoring/proctoring_pro_promo';
-$context = [
-    'promobanner_style' => "background-image: url('" . $OUTPUT->image_url('proctoring_pro_logo', 'quizaccess_proctoring') . "');",
-    'proctoring_pro_gif' => $proctoringprogif,
-    'proctoring_gif' => $proctoringgif,
-    'elearninglogo' => 'https://elearning23.com/wp-content/uploads/2023/06/cropped-e-Learning-23-logo-1-300x51.png',
-    'moodlepartnerlogo' => 'https://elearning23.com/wp-content/uploads/2024/10/moodleheader-logo.webp',
-    'proctoringdetails' => 'https://elearning23.com/moodle-proctoring-pro-details/',
-    'successsign' => $successsign,
-    'crossign' => $crossign,
-];
+    /**
+     * Proctoring Summary for the quizaccess_proctoring plugin.
+     *
+     * @package    quizaccess_proctoring
+     * @copyright  2024 Brain Station 23
+     * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later.
+     */
 
 
-echo $OUTPUT->render_from_template($template, $context);
+    require_once(__DIR__.'/../../../../config.php');
+    require_once($CFG->dirroot.'/mod/quiz/accessrule/proctoring/lib.php');
 
-echo $OUTPUT->footer();
+    require_login();
+    if (is_siteadmin()) {
+        $PAGE->set_pagelayout('admin');
+    } else {
+        $PAGE->set_pagelayout('course');
+    }
+    $courseid = optional_param('courseid', '0', PARAM_INT);
+    $cmid = optional_param('cmid' , '0' , PARAM_INT);
+
+    $PAGE->set_url(new moodle_url('/mod/quiz/accessrule/proctoring/proctoring_pro_promo.php', [
+        'courseid' => $courseid, 'cmid' => $cmid]));
+    $PAGE->set_context(context_system::instance());
+
+    $PAGE->requires->css('/mod/quiz/accessrule/proctoring/styles.css');
+    $PAGE->set_title(get_string('proctoring_pro_promo', 'quizaccess_proctoring'));
+    $PAGE->navbar->add(get_string('proctoring_pro_promo', 'quizaccess_proctoring'));
+
+    if ($courseid != 0 && $cmid != 0) {
+        $PAGE->navbar->add(get_string('reportpage', 'quizaccess_proctoring'),
+        new moodle_url('/mod/quiz/accessrule/proctoring/report.php', ['cmid' => $cmid , 'courseid' => $courseid]));
+        $PAGE->navbar->add('Proctoring Pro Promo');
+    } else {
+        $PAGE->navbar->add(get_string('userlist', 'quizaccess_proctoring'),
+        new moodle_url('/mod/quiz/accessrule/proctoring/userslist.php'));
+        $PAGE->navbar->add('Proctoring Pro Promo');
+    }
+
+    echo $OUTPUT->header();
+
+    $featuresimageurl = $OUTPUT->image_url('proctoring_pro_features', 'quizaccess_proctoring');
+    $proctoringprologo = $OUTPUT->image_url('proctoring_pro_logo', 'quizaccess_proctoring');
+    $proctoringprogif = $OUTPUT->image_url('proctoring_pro_report', 'quizaccess_proctoring');
+    $proctoringgif = $OUTPUT->image_url('proctoring_report', 'quizaccess_proctoring');
+    $successsign = $OUTPUT->image_url('success', 'quizaccess_proctoring');
+    $crossign = $OUTPUT->image_url('crossign', 'quizaccess_proctoring');
+
+    $template = 'quizaccess_proctoring/proctoring_pro_promo';
+    $context = [
+        'promobanner_style' => "background-image: url('"
+                               . $OUTPUT->image_url('proctoring_pro_logo', 'quizaccess_proctoring') . "');",
+        'proctoring_pro_gif' => $proctoringprogif,
+        'proctoring_gif' => $proctoringgif,
+        'elearninglogo' => 'https://elearning23.com/wp-content/uploads/2023/06/cropped-e-Learning-23-logo-1-300x51.png',
+        'moodlepartnerlogo' => 'https://elearning23.com/wp-content/uploads/2024/10/moodleheader-logo.webp',
+        'proctoringdetails' => 'https://elearning23.com/moodle-proctoring-pro-details/',
+        'successsign' => $successsign,
+        'crossign' => $crossign,
+    ];
+
+
+    echo $OUTPUT->render_from_template($template, $context);
+
+    echo $OUTPUT->footer();
 
