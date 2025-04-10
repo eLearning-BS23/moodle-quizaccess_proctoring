718e5d70652db2eb99d57500dee0eaf2d5e006fa
Mozahad1563 <mozahad@brainstation-23.com>
2025-04-10 16:52:00 +0600

    js file refactored

--- amd/src/startAttempt.js
+++ amd/src/startAttempt.js
@@ -1,5 +1,28 @@
-define(['jquery', 'core/ajax', 'core/notification'],
-    function($, Ajax, Notification) {
+define(['jquery', 'core/ajax', 'core/notification', 'core/str'],
+    function($, Ajax, Notification, Str) {
+        const loadStrings = async function() {
+            const stringkeys = [
+                {key: 'facematched', component: 'quizaccess_proctoring'},
+                {key: 'photonotuploaded', component: 'quizaccess_proctoring'},
+                {key: 'invalidapi', component: 'quizaccess_proctoring'},
+                {key: 'facenotmatched', component: 'quizaccess_proctoring'},
+                {key: 'wrong_during_taking_image', component: 'quizaccess_proctoring'},
+            ];
+            try {
+                const strings = await Str.get_strings(stringkeys);
+                return {
+                    facematched: strings[0],
+                    photonotuploaded: strings[1],
+                    invalidapi: strings[2],
+                    facenotmatched: strings[3],
+                    wrongduringtakingimage: strings[4],
+                };
+            } catch (error) {
+                Notification.exception(error);
+                return {}; // Return an empty object in case of an error.
+            }
+        };
+
         // Function to draw image from the box data.
         const extractFaceFromBox = async(imageRef, box, croppedImage) => {
             const regionsToExtract = [
@@ -9,11 +32,7 @@ define(['jquery', 'core/ajax', 'core/notification'],
             // eslint-disable-next-line no-undef
             let faceImages = await faceapi.extractFaces(imageRef, regionsToExtract);
 
-            if (faceImages.length === 0) {
-                // eslint-disable-next-line no-console
-               // console.log('Face not found');
-            } else {
-                // eslint-disable-next-line no-console
+            if (faceImages.length !== 0) {
                 faceImages.forEach((cnv) => {
                     croppedImage.src = cnv.toDataURL();
                 });
@@ -22,17 +41,17 @@ define(['jquery', 'core/ajax', 'core/notification'],
         const detectface = async(input, croppedImage) => {
             // eslint-disable-next-line no-undef
             const output = await faceapi.detectAllFaces(input);
-            if (output.length === 0) {
-                // eslint-disable-next-line no-console
-            } else {
-                // eslint-disable-next-line no-console
+            if (output.length !== 0) {
                 let detections = output[0].box;
                 await extractFaceFromBox(input, detections, croppedImage);
             }
         };
         return {
             setup: async function(props, modelurl) {
-                if (modelurl != null) {
+                const strings = await loadStrings(); // Load localized strings.
+
+                if (modelurl !== null) {
+                    // eslint-disable-next-line no-undef
                     await faceapi.nets.ssdMobilenetv1.loadFromUri(modelurl);
                 }
 
@@ -46,7 +65,7 @@ define(['jquery', 'core/ajax', 'core/notification'],
                     const context = canvas.getContext('2d');
                     canvas.width = props.imagewidth;
 
-                    canvas.height = canvas.width / (4/3);
+                    canvas.height = canvas.width / (4 / 3);
                     context.drawImage(video, 0, 0, canvas.width, canvas.height);
                     var data = canvas.toDataURL('image/png');
                     photo.setAttribute('src', data);
@@ -57,20 +76,16 @@ define(['jquery', 'core/ajax', 'core/notification'],
 
                     // Getting the face image from screenshot.
                     let croppedImage = $('#validate-cropimg');
-                    if (modelurl != null) {
+                    if (modelurl !== null) {
                         await detectface(photo, croppedImage);
                     }
 
                     let faceFound;
                     let faceImage;
                     if (croppedImage.src) {
-                        // eslint-disable-next-line no-console
-                        // console.log("Face found");
                         faceFound = 1;
                         faceImage = croppedImage.src;
                     } else {
-                        // eslint-disable-next-line no-console
-                        // console.log("Face not found");
                         faceFound = 0;
                         faceImage = "";
                     }
@@ -96,25 +111,24 @@ define(['jquery', 'core/ajax', 'core/notification'],
                             var status = res.status;
                             if (status === 'success') {
                                 $("#video").css("border", "10px solid green");
-                                $("#face_validation_result").html('<span style="color: green">Face Matched</span>');
+                                $("#face_validation_result").html(`<span style="color: green">${strings.facematched}</span>`);
                                 document.getElementById("fcvalidate").style.display = "none";
                                 $("#form_activate").css("visibility", "visible");
                             } else if (status === 'photonotuploaded') {
                                 $("#video").css("border", "10px solid red");
-                                // eslint-disable-next-line max-len
-                                $("#face_validation_result").html('<span style="color: red">Photo not uploaded.Please contact to the admin</span>');
-                            } else if(status === 'invalidApi') {
+                                $("#face_validation_result").html(`<span style="color: red">${strings.photonotuploaded}</span>`);
+                            } else if (status === 'invalidApi') {
                                 $("#video").css("border", "10px solid red");
-                                $("#face_validation_result").html('<span style="color: red">Invalid API.Please contact to the admin.</span>');     
+                                $("#face_validation_result").html(`<span style="color: red">${strings.invalidapi}</span>`);
                             } else {
                                 $("#video").css("border", "10px solid red");
-                                $("#face_validation_result").html('<span style="color: red">Face not matched</span>');
+                                $("#face_validation_result").html(`<span style="color: red">${strings.photonotuploaded}</span>`);
                             }
-                        }  else {
+                        } else {
                             document.getElementById('loading_spinner').style.display = 'none';
                             if (video) {
                                 Notification.addNotification({
-                                    message: 'Something went wrong during taking the image.',
+                                    message: strings.wrongduringtakingimage,
                                     type: 'error'
                                 });
                             }
